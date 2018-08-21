cf9cecedbacf8d5aba2696dbe7d26dd64ccea4c5
Abu Safian Blay <41937724+pierce2r@users.noreply.github.com>
2018-08-21 08:03:31 -0400

    Add files via upload

---
+++ ip-mapper.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python 2.x
+import requests
+import sys
+import urllib
+import pyperclip
+
+try:
+	ip_address=raw_input('Enter IP:')
+	main_api = ('https://api.ip2location.com/?ip='+str(ip_address)+'&key=*****&package=WS6') #replace ****** with your API key
+	response = requests.get(main_api)
+
+  	print('*'*80)
+  	print('Created by Pierce2r').center(80)
+	print('Version 1.0').center(80)
+  	print('*'*80)
+		
+	if response.status_code==200:
+		print('\n[*] Tracking '+str(ip_address)+' for credentials...')
+		browser=urllib.urlopen(main_api)
+		page=browser.read()
+		pyperclip.copy(page)
+		paste=pyperclip.paste()
+		line=paste.split(';')
+		print('[+]Country name: '+line[1])
+		print('[+]Region name: '+line[2])
+		print('[+]City name: '+line[3])
+		print('[+]Latitude: '+line[4])
+		print('[+]Longitude '+line[5])
+		print('[+]ISP name: '+line[6]+ '\n')
+
+except KeyboardInterrupt:
+	sys.exit()
+
